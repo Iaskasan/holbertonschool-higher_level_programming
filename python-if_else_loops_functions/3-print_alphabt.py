e3b4a933f788d6726f3e916d9d47a031000e1d07
Iaskasan <nokiesan@yahoo.fr>
2024-01-22 16:04:06 +0100

    doube if

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for ascii_alphabet in range(97, 123):
+    if ascii_alphabet != 101:
+        if ascii_alphabet != 113:
+            print("{}".format(chr(ascii_alphabet)), end="")
