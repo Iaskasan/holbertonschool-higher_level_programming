afa616c04d67206abb0a8bb56903c05eeb94700a
Iaskasan <nokiesan@yahoo.fr>
2024-01-24 13:03:24 +0100

    test uppcase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase_test(input_str):
+    result_str = ""
+
+    for char in input_str:
+        ascii_val = ord(char)
+        if 97 <= ascii_val <= 122:
+            uppercase_char = chr(ascii_val - 32)
+            result_str += uppercase_char
+        else:
+            result_str += char
+
+    print(result_str, end='\n')
