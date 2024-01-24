6eba824cca823e480db77dba1942103c107d34a9
Iaskasan <nokiesan@yahoo.fr>
2024-01-24 11:03:14 +0100

    .forlmat

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print(f"{i}\n")
+        print("{}\n".format(i))
     if i < 99:
-        print(f"{i:02d}", end=", ")
+        print("{:02d}".format(i), end=", ")
