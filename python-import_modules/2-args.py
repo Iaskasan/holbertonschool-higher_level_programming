0e557846cca949854c8ec7e794074629a94da880
Iaskasan <nokiesan@yahoo.fr>
2024-01-25 12:36:57 +0100

    fixed syntax

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     argc = sys.argv
     if (len(argc) - 1) == 0:
-        print("{} argument.".format(len(argc) - 1))
+        print("{} arguments.".format(len(argc) - 1))
     elif (len(argc) - 1) < 2:
         print("{} argument:".format(len(argc) - 1))
     else:
