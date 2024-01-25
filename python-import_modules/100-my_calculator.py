e026aa0615c22987c4444c4f5f81b600b419b924
Iaskasan <nokiesan@yahoo.fr>
2024-01-25 20:40:16 +0100

    pycode and .format

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,20 +4,22 @@ import sys
 
 if __name__ == "__main__":
     argv = sys.argv
+    a = int(argv[1])
+    b = int(argv[3])
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if argv[2] == '+':
-        print(f"{argv[1]} + {argv[3]} = {add(int(argv[1]), int(argv[3]))}")
+        print("{} + {} = {}".format(a, b, add(a, b)))
         exit(0)
     if argv[2] == '-':
-        print(f"{argv[1]} - {argv[3]} = {sub(int(argv[1]), int(argv[3]))}")
+        print("{} - {} = {}".format(a, b, sub(a, b)))
         exit(0)
     if argv[2] == '*':
-        print(f"{argv[1]} * {argv[3]} = {mul(int(argv[1]), int(argv[3]))}")
+        print("{} * {} = {}".format(a, b, mul(a, b)))
         exit(0)
     if argv[2] == '/':
-        print(f"{argv[1]} / {argv[3]} = {div(int(argv[1]), int(argv[3]))}")
+        print("{} / {} = {}".format(a, b, div(a, b)))
         exit(0)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
