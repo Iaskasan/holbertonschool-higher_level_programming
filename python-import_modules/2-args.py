1d8506ca9e0b9540483d1eabdb67ec951b59db9b
Iaskasan <nokiesan@yahoo.fr>
2024-01-25 11:19:19 +0100

    added args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argc = sys.argv
+    if (len(argc) - 1) < 2:
+        print("{} argument:".format(len(argc) - 1))
+    else:
+        print("{} arguments:".format(len(argc) - 1))
+    for i in range(len(argc) - 1):
+        arg_count = i + 1
+        print("{}: {}".format(i + 1, sys.argv[arg_count]))
