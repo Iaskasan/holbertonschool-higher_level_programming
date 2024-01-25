f75908f724343fa8cf7b81f0e0c994ee36a64644
Iaskasan <nokiesan@yahoo.fr>
2024-01-25 13:10:09 +0100

    only executed when main

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
+    final_value = 0
+    args = sys.argv
 
-final_value = 0
-args = sys.argv
-
-for i in range(1, len(args)):
-    final_value += int(args[i])
-print(final_value)
+    for i in range(1, len(args)):
+        final_value += int(args[i])
+    print(final_value)
