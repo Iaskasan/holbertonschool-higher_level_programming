a172619ec4b27f94e21cb22c0068d7f0b157f9bf
Iaskasan <nokiesan@yahoo.fr>
2024-01-29 15:58:55 +0100

    added multiple return

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (None)
     lenght = len(sentence)
     first = sentence[0]
+
+    if len(sentence) == 0:
+        first = None
+        return (first)
     return (lenght, first)
