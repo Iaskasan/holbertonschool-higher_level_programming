98d004c693cc0c2a9266ed41dfe264ad6343e39e
Iaskasan <nokiesan@yahoo.fr>
2024-01-29 14:44:39 +0100

    indentation fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-                print("{:d}".format(j), end="")
-                if i[-1] != j:
-                    print(" ", end="")
+            print("{:d}".format(j), end="")
+            if i[-1] != j:
+                print(" ", end="")
         print("".format())
