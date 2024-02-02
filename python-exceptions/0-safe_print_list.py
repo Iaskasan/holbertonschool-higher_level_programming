1674777013618317b741e58abf0179ac9445f061
Iaskasan <nokiesan@yahoo.fr>
2024-02-02 12:41:30 +0100

    added safe print func

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
+        print()
+        return x
+    except IndexError:
+        count = 0
+        for i in my_list:
+            count += 1
+        print()
+        return count
