3b2a2a42ac8d3a7364cb76ed6dc0f8a80397c770
Iaskasan <nokiesan@yahoo.fr>
2024-02-05 15:55:33 +0100

    pycode

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""defines a Square class"""
+
+
+class Square:
+    """This is the class Square"""
+    pass
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This the self where you can eat"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.size ** 2
