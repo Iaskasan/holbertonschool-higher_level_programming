02b9ce0e680ce7344b93ff8f3d977f3328657979
Iaskasan <nokiesan@yahoo.fr>
2024-02-05 13:45:31 +0100

    adonisifoazjd

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+from typing import overload
+"""defines a Square class"""
+
+
+class Square:
+    """This is the class Square"""
+    pass
+
+    def __init__(self, size=0):
+        """This the self where you can eat"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
