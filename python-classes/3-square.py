8f3443b36881fd2abbfbfbbbc9247becd9703cfc
Iaskasan <nokiesan@yahoo.fr>
2024-02-05 14:06:15 +0100

    square square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""defines a Square class"""
+
+
+class Square:
+    """This is the class Square"""
+    pass
+
+    def __init__(self, size=0):
+        """This the self where you can eat"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
