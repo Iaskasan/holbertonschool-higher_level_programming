59bbc5016e18455c0096b3b1dc19437bf14eb5a5
Iaskasan <nokiesan@yahoo.fr>
2024-02-05 11:59:58 +0100

    added size param func

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""defines a Square class"""
+
+
+class Square:
+    """This is the class Square"""
+    pass
+
+    def __init__(self, size):
+        """This the self where you can eat"""
+        self.__size = size
