586bb0e6a8a70fe4cfe76ff639de7283c7919a35
Iaskasan <nokiesan@yahoo.fr>
2024-02-06 17:43:12 +0100

    added square print func

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""defines a Square class"""
+
+
+class Square:
+    """This is the class Square"""
+    pass
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def area(self):
+        return self.size ** 2
+
+
+    @size.setter
+    def size(self, value):
+        """This the self where you can eat"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            print("#" * self.size)
