36d1ce93ccda04ba8a7a58a520b93346b813cd58
Iaskasan <nokiesan@yahoo.fr>
2024-02-08 15:09:13 +0100

    added square func

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Square print"""
+
+
+def print_square(size):
+    """prints a square"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
