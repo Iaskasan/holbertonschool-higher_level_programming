248edd5ad83d1e3f829ea34a6fdd9661aba8e4fc
Iaskasan <nokiesan@yahoo.fr>
2024-02-08 10:15:43 +0100

    added docstrings

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""module adds integer"""
+
+
 def add_integer(a, b=98):
+    """adds 2 integers together"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
