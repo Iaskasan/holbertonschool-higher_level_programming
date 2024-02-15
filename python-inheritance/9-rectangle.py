439798005e9b6845bc248ced384d9e9272b81ee9
Iaskasan <nokiesan@yahoo.fr>
2024-02-15 20:02:46 +0100

    forgot private attribs

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -18,4 +18,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return f"[Rectangle] {self.width}/{self.height}"
+        return f"[Rectangle] {self.__width}/{self.__height}"
