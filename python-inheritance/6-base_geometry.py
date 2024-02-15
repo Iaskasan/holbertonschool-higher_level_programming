07451b7cf4d41c58f28d225d9b8b8ee6a0689e86
Iaskasan <nokiesan@yahoo.fr>
2024-02-15 19:19:30 +0100

    added area error mess

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module docstring"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """area method doc"""
+        raise Exception("area() is not implemented")
