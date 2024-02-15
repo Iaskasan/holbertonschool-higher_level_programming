3998f8bf9209c63fb871d0a0f247cf2e7bb76054
Iaskasan <nokiesan@yahoo.fr>
2024-02-15 19:58:58 +0100

    test to add func with error

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module docstring"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class kek"""
+    def __init__(self, width, height):
+        """docstring checker"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.width}/{self.height}"
