5e7943303ca1020cc633dba77bfb82f5ac52c8b2
Iaskasan <nokiesan@yahoo.fr>
2024-02-15 19:48:11 +0100

    test format

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 """module docstring"""
 
 
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """area method doc"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates the types"""
-        if type(value) is not (int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
