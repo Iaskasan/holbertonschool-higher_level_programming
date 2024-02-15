0ba87d04fce3beba0e95909ebb1a034a73559954
Iaskasan <nokiesan@yahoo.fr>
2024-02-15 19:42:53 +0100

    added class rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module docstring"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """area method doc"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates the types"""
+        if type(value) is not (int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
