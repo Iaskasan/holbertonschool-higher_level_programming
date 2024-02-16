5f1c6b832aff9a6b9b43782b427ee7f20e392117
Iaskasan <nokiesan@yahoo.fr>
2024-02-16 14:34:42 +0100

    added loads func

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""oui oui oui"""
+
+import json
+
+
+def from_json_string(my_str):
+    """_summary_
+
+    Args:
+        my_str (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return json.loads(my_str)
