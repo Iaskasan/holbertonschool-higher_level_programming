2abd9ea18ab2a75dd513d05832d6d00e9e2e15f6
Iaskasan <nokiesan@yahoo.fr>
2024-02-18 15:34:20 +0100

    added the class student base

---
+++ python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""oui oui oui"""
+
+
+class Student:
+    """class Student doc"""
+    def __init__(self, first_name, last_name, age):
+        self.fname = first_name
+        self.lname = last_name
+        self.age = age
+
+    def to_json(self):
+        return Student.__dict__
