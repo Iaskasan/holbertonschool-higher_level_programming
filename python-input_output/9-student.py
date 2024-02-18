7b3ad0ca13b8d2f311294f91e7d5d7625519c394
Iaskasan <nokiesan@yahoo.fr>
2024-02-18 15:36:49 +0100

    modified instances names?

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,8 +5,8 @@
 class Student:
     """class Student doc"""
     def __init__(self, first_name, last_name, age):
-        self.fname = first_name
-        self.lname = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
