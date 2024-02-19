27665e42f9572bca03b9052fca227b23711ce10c
Iaskasan <nokiesan@yahoo.fr>
2024-02-19 15:33:28 +0100

    fixed if/else syntax

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,17 +79,19 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """update args"""
-        self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
 
-        if args is None:
+        else:
             if "id" in kwargs:
                 self.id = kwargs["id"]
             if "width" in kwargs:
