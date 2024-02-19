945210a8011f42b4e42ed5699c69c152e86a881b
Iaskasan <nokiesan@yahoo.fr>
2024-02-19 12:12:10 +0100

    added the base of the rectanlge class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""rectangle class file"""
+from base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle doc"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self):
+        pass
+
+    @height.setter
+    def height(self):
+        pass
+
+    @x.setter
+    def x(self):
+        pass
+
+    @y.setter
+    def y(self):
+        pass
