ded357fa879bc37cf0843eaec195bbad2ac4e0eb
Iaskasan <nokiesan@yahoo.fr>
2024-02-19 16:01:34 +0100

    added square base

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""square class file"""
+from models.rectangle import Rectangle
+"""square class file"""
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, id, x, y)
+    
+    @property
+    def size(self):
+        return self.__height
+    
+    @property
+    def size(self):
+        return self.__width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - \
+{self.size}"
