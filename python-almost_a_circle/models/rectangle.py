e39743c662a9c3c1825fd849921c451348a413aa
Iaskasan <nokiesan@yahoo.fr>
2024-02-19 13:57:59 +0100

    changed the init attrs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,10 +8,10 @@ class Rectangle(Base):
     """class Rectangle doc"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """rectangle class file"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
