01f6b5bce68b854047317c758785c19fb09f749e
Iaskasan <nokiesan@yahoo.fr>
2024-02-19 15:16:31 +0100

    added args handling

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,15 @@ class Rectangle(Base):
         """the __str__ method"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
 {self.width}/{self.height}"
+
+    def update(self, *args):
+        """update args"""
+        self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
