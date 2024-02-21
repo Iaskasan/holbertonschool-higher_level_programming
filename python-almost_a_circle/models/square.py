e4acb7da6bc2952164ec86fec0f9b91e398f4965
Iaskasan <nokiesan@yahoo.fr>
2024-02-21 12:23:02 +0100

    fixed square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,15 +11,11 @@ class Square(Rectangle):
         Rectangle (_type_): _description_
     """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, id, x, y)
+        super().__init__(size, size, x, y, id)
     
     @property
     def size(self):
         return self.__height
-    
-    @property
-    def size(self):
-        return self.__width
     
     @size.setter
     def size(self, value):
