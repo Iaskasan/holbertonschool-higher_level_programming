fe389f051112e9a38f946547514246221cd3b64c
Iaskasan <nokiesan@yahoo.fr>
2024-02-21 12:09:13 +0100

    added docstrings

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,6 +5,11 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """_summary_
+
+    Args:
+        Rectangle (_type_): _description_
+    """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, id, x, y)
     
@@ -22,5 +27,10 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         return f"[Square] ({self.id}) {self.x}/{self.y} - \
 {self.size}"
