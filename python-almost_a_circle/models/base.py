d0fa49fabf2147f6c29c823104dcec1e583631bd
Iaskasan <nokiesan@yahoo.fr>
2024-02-21 15:13:03 +0100

    added json to string to class base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """base class file"""
-
+from json import dumps, loads
 
 class Base:
     """class Based"""
@@ -12,3 +12,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """faut pas copier les githubs"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
