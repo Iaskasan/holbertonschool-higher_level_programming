8fd1c63ba2d9efae421e6699224f3c5374430a63
Iaskasan <nokiesan@yahoo.fr>
2024-05-06 20:33:49 +0200

    fixed the missing blank space at the end

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,7 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i == 100:
-            print("Buzz")
-        elif i % 15 == 0:
+        if i % 15 == 0:
             print("FizzBuzz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
@@ -12,3 +10,6 @@ def fizzbuzz():
             print("Fizz", end=" ")
         else:
             print(i, end=" ")
+    print()
+    
+fizzbuzz()
