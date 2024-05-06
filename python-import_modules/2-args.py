dab7189569afb2f4fae39b1af7ba4a50c5319dfa
Iaskasan <nokiesan@yahoo.fr>
2024-05-06 21:40:32 +0200

    added the argument printing function

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv)))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv)))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments.".format(len(argv)))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
