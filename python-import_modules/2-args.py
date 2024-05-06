293639bc49715b618c810c1bb7efe6cbebf8e2da
Iaskasan <nokiesan@yahoo.fr>
2024-05-06 21:42:04 +0200

    removed the script counting as an arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) == 1:
-        print("{} arguments.".format(len(argv)))
+        print("{} arguments.".format(len(argv) - 1))
     elif len(argv) == 2:
-        print("{} argument:".format(len(argv)))
+        print("{} argument:".format(len(argv) - 1))
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments.".format(len(argv)))
+        print("{} arguments.".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
