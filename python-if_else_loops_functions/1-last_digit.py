77e72acac305f6f836a55c0096092a41449efbb3
Iaskasan <nokiesan@yahoo.fr>
2024-05-06 15:27:29 +0200

    removed blank space befor the 0 prompt

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,6 +7,6 @@ if number < 0:
 if last_dig > 5:
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
 elif last_dig == 0:
-    print(f" Last digit of {number} is {last_dig} and is 0")
+    print(f"Last digit of {number} is {last_dig} and is 0")
 elif last_dig < 5:
     print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
