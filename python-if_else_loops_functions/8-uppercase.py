290719849b104fd2fc577ff5886cc98efdf7cd0f
Iaskasan <nokiesan@yahoo.fr>
2024-05-06 17:31:22 +0200

    added uppercase function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def uppercase(c):
     for char in c:
-        ascii_value = ord(char)
-        if ascii_value > 96:
-            ascii_value -= 32
-        print(chr(ascii_value), end="".format())
-    print("\n", end="".format())
-
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+        ascii = ord(char)
+        if ascii > 96:
+            ascii -= 32
+        print(chr(ascii), end='')
+    print()
