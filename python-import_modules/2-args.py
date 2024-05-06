a7cfa49c3374e79d5df2ffc61457a65e22c25196
Iaskasan <nokiesan@yahoo.fr>
2024-05-06 21:43:00 +0200

    fixed print syntaxe . > :

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("{} argument:".format(len(argv) - 1))
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments.".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
