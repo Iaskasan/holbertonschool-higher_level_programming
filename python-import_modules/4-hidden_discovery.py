f852bf8d53f6d6900fd2398ababf4691e9cfb8f1
Iaskasan <nokiesan@yahoo.fr>
2024-05-07 14:09:20 +0200

    test with Fab's code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 import hidden_4
 
 if __name__ == "__main__":
-    for att in dir(hidden_4):
-        if att[:2] != "__":
-            print(att)
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
