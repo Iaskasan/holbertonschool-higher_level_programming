2b67311c03f99d99ad613762db7fffb571cde999
Iaskasan <nokiesan@yahoo.fr>
2024-05-07 18:17:54 +0200

    trying some voodoo

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,9 @@
 import hidden_4
 
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if name[:2] != "__":
-            print(name)
+    try:
+        for name in dir(hidden_4):
+            if name[:2] != "__":
+                print(name)
+    except ImportError:
+        pass
