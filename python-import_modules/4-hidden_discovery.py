ebd53fd997793a6757b33a50cfe93f0cdb6f18ef
Iaskasan <nokiesan@yahoo.fr>
2024-05-07 10:35:54 +0200

    Attempting to fix the bad magic error

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 import hidden_4
 
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if not name.startswith('__'):
-            print(name)
+    for att in dir(hidden_4):
+        if not att.startswith("__"):
+            print(att)
