42f1ca0cf4a9239b4a46c672851c7e68b914b930
Iaskasan <nokiesan@yahoo.fr>
2024-05-09 10:52:33 +0000

    added hidden discovery via sandbox

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,6 @@
 import hidden_4
 
 if __name__ == "__main__":
-    try:
-        for name in dir(hidden_4):
-            if name[:2] != "__":
-                print(name)
-    except ImportError:
-        pass
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
