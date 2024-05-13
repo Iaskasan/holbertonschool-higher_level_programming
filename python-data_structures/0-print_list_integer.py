18f405dba8fbe9fbb2ad00f78c5f30462826e371
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 10:30:34 +0200

    added 4th task and fixed the first one

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for elements in my_list:
-        print("{}".format(elements))
+        print("{:d}".format(elements))
--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+        my_list = my_list[::-1]
+        for elements in my_list:
+            print("{:d}".format(elements))
