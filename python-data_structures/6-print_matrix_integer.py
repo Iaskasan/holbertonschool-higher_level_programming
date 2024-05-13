2ced552961be931a374ea8c07c68b2edea48a4b7
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 11:58:07 +0200

    added the matrix function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = len(matrix[0])
+    for j in range(i):
+        for k in range(i):
+            if k == i - 1:
+                print("{}".format(matrix[j][k]))
+            else:
+                print("{}".format(matrix[j][k]), end=", ")
