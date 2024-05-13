9a10ff699d9385a92526f383ebe08afe1a2d273f
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 15:03:30 +0200

    tried to convert into tuple

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,4 +2,9 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         return(len(sentence), None)
-    return(len(sentence), sentence[0])
+    return tuple((len(sentence), sentence[0]))
+
+
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
