9b3012aba7622fb014ae27a28783a01451441ee4
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 22:17:09 +0200

    added the roman numberrrrr mùethod

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return None
+    dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+            }
+    result = 0
+    str_list = list(roman_string)
+    Bool = False
+    for i in range(len(str_list)):
+        if len(str_list) > 1:
+            if str_list[i - 1] != "I" and str_list[-1] != "I":
+                Bool = True
+        for key, value in dict.items():
+            if Bool:
+                result -= 1
+                Bool = False
+            elif key == str_list[i] and Bool is False:
+                result += value
+    return result
