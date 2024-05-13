81a899459bdf97211da1fea252ec463ce3c23e68
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 18:09:00 +0200

    removed the test code...

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -8,7 +8,3 @@ def uniq_add(my_list=[]):
         if my_list[i] != my_list[i - 1]:
             a += my_list[i]
     return a
-    
-my_list = [1, 1, 2]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
