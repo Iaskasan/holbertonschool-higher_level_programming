1a6fbd7bbd8e8ee18716754407485c69d18779dc
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 18:05:30 +0200

    handled the all same number in the list case

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     my_list.sort()
-    a = 0
-    for i in range(len(my_list)):
+    a = my_list[0]
+    for i in range(1, len(my_list)):
         if my_list[i] != my_list[i - 1]:
             a += my_list[i]
     return a
