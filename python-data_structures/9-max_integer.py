c0f6f99edd9977501b450b743172dff6c8881338
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 15:51:07 +0200

    addef the max integer function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    a = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] >= a:
+            a = my_list[i]
+    return a
