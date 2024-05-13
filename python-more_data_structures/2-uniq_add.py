53544a76db1c0c9a2b4404062f4aed2b5d974d3a
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 18:08:22 +0200

    handled the empty list case

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     my_list.sort()
+    if len(my_list) == 0:
+        return 0
     a = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] != my_list[i - 1]:
             a += my_list[i]
     return a
+    
+my_list = [1, 1, 2]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
