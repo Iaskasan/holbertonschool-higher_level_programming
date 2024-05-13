577b95dde71b0dbff4d92289d35ed77b5fe95dce
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 18:00:32 +0200

    added the unique addition method

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    my_list.sort()
+    a = 0
+    for i in range(len(my_list)):
+        if my_list[i] != my_list[i - 1]:
+            a += my_list[i]
+    return a
