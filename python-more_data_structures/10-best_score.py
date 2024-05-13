c9acd3ae48bc11537053f8af3e75427bdc7168be
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 21:19:58 +0200

    improved the code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    sorted_dic = sorted(a_dictionary.values())
-    last_val = int(sorted_dic[-1])
-    for key, value in a_dictionary.items():
-        if value == last_val:
-            return key
+    return max(a_dictionary, key=a_dictionary.get)
