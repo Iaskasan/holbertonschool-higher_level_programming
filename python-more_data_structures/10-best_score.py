37c8cc7e4cff96933db626b2fb81f96ca9e82d0c
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 21:14:30 +0200

    added the best score method

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    sorted_dic = sorted(a_dictionary.values())
+    last_val = int(sorted_dic[-1])
+    for key, value in a_dictionary.items():
+        if value == last_val:
+            return key
