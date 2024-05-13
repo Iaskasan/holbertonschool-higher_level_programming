77767820e5485e7633ae0e7f43c104f5b9306150
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 12:23:46 +0200

    removed the ,

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) < 2:
-        print("".format())
+        print()
     i = len(matrix[0])
     for j in range(i):
         for k in range(i):
             if k == i - 1:
                 print("{:d}".format(matrix[j][k]))
             else:
-                print("{:d}".format(matrix[j][k]), end=", ")
+                print("{:d}".format(matrix[j][k]), end=" ")
