b4aed7ad5ce50ac6c305c78706bcdf82aa886936
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 18:13:28 +0200

    cleaned the code

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_list.sort()
-    if len(my_list) == 0:
-        return 0
-    a = my_list[0]
-    for i in range(1, len(my_list)):
-        if my_list[i] != my_list[i - 1]:
-            a += my_list[i]
-    return a
+    return sum(set(my_list))
