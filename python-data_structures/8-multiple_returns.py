570706569241270caa9a523d0ac75ecaa605d2e2
Iaskasan <nokiesan@yahoo.fr>
2024-05-13 15:03:42 +0200

    removed the test code

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,8 +3,3 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return(len(sentence), None)
     return tuple((len(sentence), sentence[0]))
-
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
