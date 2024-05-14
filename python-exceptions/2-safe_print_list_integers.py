2589a6236abcb3366fd74ce19d7625cdc14e15e5
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 11:56:40 +0200

    handlded the x wrong type

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError):
-            i -= 1
-            continue
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+            except ValueError:
+                i -= 1
+                continue
+    except TypeError:
+        pass
     print()
     return i
