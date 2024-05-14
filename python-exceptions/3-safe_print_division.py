adba752439ef2a4235de0e87b54c0fb8e3798cd5
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 13:46:44 +0200

    added the division function

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = 0
+        c = a / b
+    except (UnboundLocalError, ZeroDivisionError):
+        pass
+    finally:
+        print("Inside result: {}".format(c))
+        if c == 0:
+            return None
+        return c
