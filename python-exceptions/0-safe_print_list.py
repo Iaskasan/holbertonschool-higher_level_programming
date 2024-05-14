46be34e273fb9cb6f379f122e4680e48ef24ef64
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 11:25:18 +0200

    fixed the x = 0 case

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
@@ -7,4 +8,4 @@ def safe_print_list(my_list=[], x=0):
             print()
             return i
     print()
-    return i
+    return x
