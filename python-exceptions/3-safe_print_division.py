6fef7b09d4c6e0ca09e6f6618013add5191c4dc9
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 13:50:33 +0200

    fixed a case issue

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        c = 0
         c = a / b
     except (UnboundLocalError, ZeroDivisionError):
-        pass
+        c = None
     finally:
         print("Inside result: {}".format(c))
-        if c == 0:
-            return None
         return c
