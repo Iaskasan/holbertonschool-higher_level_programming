a0c372a0fbbb2fa41074a85c0c203f6d62ec66fa
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 15:50:18 +0200

    updated the square class with the method area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""building the class square"""
+
+
+class Square:
+    """This is the class Square
+
+    for now its an empty classs
+    """
+    def __init__(self, size=0):
+        """this is the constructor"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """square the size of the square and returns an int"""
+        return self.__size ** 2
