9fe7743b68b564f7bd1f72cc38377e647d7f2fb2
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 15:36:00 +0200

    added the errors handling

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""building the class square"""
+
+
+class Square:
+    """This is the class Square
+
+    for now its an empty classs
+    """
+    def __init__(self, size=0):
+        """this is the constructor"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
