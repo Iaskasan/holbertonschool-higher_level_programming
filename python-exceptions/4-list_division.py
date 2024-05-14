26f188fd2b2b2e96a709ad5ef8f152c9cc6a9cd0
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 14:32:02 +0200

    upgraded the code by removing a useless continue

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,7 +11,6 @@ def list_division(my_list_1, my_list_2, list_length):
             except TypeError:
                 print("wrong type")
                 new_list.append(0)
-                continue
             except IndexError:
                 print("out of range")
                 new_list.append(0)
