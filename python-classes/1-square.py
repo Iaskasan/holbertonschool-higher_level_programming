c8235d930317fe17c532c3581765ac49ad41fbb9
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 15:30:27 +0200

    maybe

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""building the class square"""
+
+
+class Square:
+    """This is the class Square
+
+    for now its an empty classs
+    """
+    def __init__(self, size):
+        """this is the constructor"""
+        self.__size = size
