dc97bfbc1e7153421379f7586577dd42e919aa30
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 15:25:00 +0200

    tried longer docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """This is the class Square"""
-    def __init__(self):
-        """this is the constructor"""
-        pass
+    """This is the class Square
+
+    for now its an empty classs
+    """
+    pass
