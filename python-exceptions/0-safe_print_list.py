c865e8df422944ffefcb7ad366f3453c987a358a
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 11:18:04 +0200

    removed the use of len()

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
         except IndexError:
-            pass
+            print()
+            return i
     print()
-    return len(my_list)
+    return i
