4853ee3bff80522b9b0d0a71f7653c52ceaaa836
Iaskasan <nokiesan@yahoo.fr>
2024-05-14 11:48:34 +0200

    adde the safe printing function

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            i -= 1
+            continue
+    print()
+    return i
