b2eb43c018a3551af269d7d3d90f81ff55244c18
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 19:25:10 +0200

    added the advanced task weighted average

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    a = 0
+    b = 0
+    for score, weight in my_list:
+        a += score * weight
+        b += weight
+    return a / b
