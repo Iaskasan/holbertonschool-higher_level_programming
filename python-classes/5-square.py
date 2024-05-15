82978a2268d4354c2651b8f89f4370b9dcd26213
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 12:16:55 +0200

    added the priçnt square method

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""building the class square"""
+
+
+class Square:
+    """This is the class Square
+
+    The class is not yet complete
+    """
+    def __init__(self, size=0):
+        """this is the constructor"""
+        self.__size = size
+
+    def area(self):
+        """square the size of the square and returns an int"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        '''class method to retrieve or modify the value of size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''checks if the input is an int and > 0'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        '''prints the square with "#" as character'''
+        if self.size == 0:
+            print()
+        for _ in range(self.size):
+            print("#" * self.size)
