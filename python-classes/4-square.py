f935efa53693fd25ffca2c8a2ebbad883c8edbfa
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 12:11:08 +0200

    adde the property and proprty setter of size to the class Square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""building the class square"""
+
+
+class Square:
+    """This is the class Square
+
+    for now its an empty classs
+    """
+    def __init__(self, size=0):
+        """this is the constructor"""
+        self.__size = size
+
+    def area(self):
+        """square the size of the square and returns an int"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        '''class method to retrieve or modify the value of size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''checks if the input is an int and > 0'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
