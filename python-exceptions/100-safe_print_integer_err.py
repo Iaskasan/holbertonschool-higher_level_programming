bc5616ef5dfb4c4e636d000b1c55a6fadcda3e35
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 22:50:39 +0200

    cahnged default print to stderr.write() to print in stderr stream

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+from sys import stderr
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Exception:", err)
+        stderr.write(f"Exception: {err}\n")
         return False
