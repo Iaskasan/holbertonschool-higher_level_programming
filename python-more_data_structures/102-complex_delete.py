1dfa1ee482e9e469982a7c62b1c0dc080306959a
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 22:31:25 +0200

    added the complex delete function (hint: recursive !)

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for key, val in a_dictionary.items():
+        if val != value:
+            continue
+        else:
+            a_dictionary.pop(key)
+            return complex_delete(a_dictionary, value)
+    return a_dictionary
