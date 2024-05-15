43895966368a76224058958ef12b3281ea1dc8f5
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 23:16:46 +0200

    2nd attempt, added the try and finally

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,9 +2,10 @@
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        if i < a:
-            raise Exception("Too far")
-        result = a ** b / i
-        result = b + a
-        break
+        try:
+            if i < a:
+                raise Exception("Too far")
+            result += a ** b / i
+        finally:
+            result = b + a
     return result
