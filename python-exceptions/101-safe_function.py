7acbfb0988ba9f3b559d7b2b568db25c4f1cae2c
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 22:57:14 +0200

    added the safe function execution

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as err:
+        stderr.write(f"Exception: {err}\n")
+        return None
