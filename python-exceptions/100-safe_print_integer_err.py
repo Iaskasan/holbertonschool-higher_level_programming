601454fb7fba49059fdf5f0d86203ead66dfa1c9
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 22:44:04 +0200

    changed the exception error to Exception

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,5 +3,5 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except Exception as err:
         print("Exception:", err)
