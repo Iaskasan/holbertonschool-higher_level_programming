f787dcd929d0e935651b3a75e0c9ce15928ea9fd
Iaskasan <nokiesan@yahoo.fr>
2024-05-15 23:08:22 +0200

    first attempt of the bytecode decypher

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        if i < a:
+            raise Exception("Too far")
+        result = a ** b / i
+        result = b + a
+        break
+    return result
