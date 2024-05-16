dedfbceeef6fcf3ec48b4b42f8e5718ed2eff05a
Iaskasan <nokiesan@yahoo.fr>
2024-05-16 13:43:14 +0200

    added docstrings

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''Defines a a node of a singled linked list'''
 
 
 class Node:
@@ -9,10 +10,12 @@ class Node:
 
     @property
     def data(self):
+        '''returns the data, must be an int'''
         return self.__data
 
     @data.setter
     def data(self, value):
+        '''checks if the data is an int, otherwise raise a Typerror'''
         if type(value) is int:
             self.__data = value
         else:
@@ -20,10 +23,15 @@ class Node:
 
     @property
     def next_node(self):
+        '''return a node'''
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        '''
+        checks if the next_node has the right class (Node) or is None
+        and gives it to the constructor
+        '''
         if value is None or isinstance(value, Node):
             self.__next_node = value
         else:
@@ -33,13 +41,22 @@ class Node:
 class SinglyLinkedList:
     '''This class defines a singly linked list'''
     def __init__(self):
+        '''Basic constructor that initialises an empty list'''
         self.list = []
 
     def sorted_insert(self, value):
+        '''
+        takes a value as input and adds it to a list
+        before sorting the list
+        '''
         self.list.append(value)
         self.list = sorted(self.list)
 
     def __str__(self) -> str:
+        '''the __str__ method that returns each elements of
+        the list followed by a new line
+        output = str
+        '''
         str_print = ""
         lenght = len(self.list)
         for i in range(lenght):
