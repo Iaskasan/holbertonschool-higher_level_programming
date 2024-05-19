06203ce7b52ae778d2423b29d7143923bb3ddccb
Iaskasan <nokiesan@yahoo.fr>
2024-05-19 09:01:54 +0200

    changed the exception handling

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -3,10 +3,9 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if i < a:
+            if i > a:
                 raise Exception("Too far")
             result += a ** b / i
-        finally:
-            result = b + a
-            break
+        except Exception:
+            result = a + b
     return result
