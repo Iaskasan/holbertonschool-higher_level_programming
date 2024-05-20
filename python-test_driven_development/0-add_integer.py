206085f1cdf06ee7df14a45f547048125c22f082
Iaskasan <nokiesan@yahoo.fr>
2024-05-20 11:11:21 +0200

    added the test file and the add integer module

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'''Module for the method add_integer'''
+
+
+def add_integer(a, b=98):
+    '''A simple function that returns the sum of a and b'''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
