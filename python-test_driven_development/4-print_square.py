c9bd60c18e18b69f558f553cbad62dcbf08f3dd9
Iaskasan <nokiesan@yahoo.fr>
2024-05-20 13:09:56 +0200

    added the print square function and its tests and modified the title of say my name's txt file

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''a function that prints a square with a given size'''
+
+
+def print_square(size):
+    '''prints a square with # as character'''
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size > 0:
+        raise TypeError("size must be an integer")
+    for _ in range(size):
+        print("#" * size)
