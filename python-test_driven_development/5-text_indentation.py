16b694a11b2b2e6f6b0aca2ba3a4062185484d8d
Iaskasan <nokiesan@yahoo.fr>
2024-05-20 13:39:50 +0200

    added the text indentation function and its tests

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''a function that indent a text after each . : or ? character'''
+
+
+def text_indentation(text):
+    '''indent the input string by adding new lines and removing spaces'''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    next_is_space = False
+    for char in text:
+        if char in (".", "?", ":"):
+            print(char)
+            print()
+            next_is_space = True
+        elif next_is_space:
+            next_is_space = False
+            continue
+        else:
+            print(char, end="")
