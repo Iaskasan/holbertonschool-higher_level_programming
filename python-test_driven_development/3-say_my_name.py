081f991b5920c3f73fff4e5767581b6d8986a698
Iaskasan <nokiesan@yahoo.fr>
2024-05-20 12:57:06 +0200

    added the say my name function and its tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'''method that prints the first name and last name'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''simply prints the nfirst_name and last_name (if any)'''
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
