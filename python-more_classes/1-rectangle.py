609fad833e3622571c4b4cc3f92f6a381c3d9816
Iaskasan <nokiesan@yahoo.fr>
2024-05-21 11:20:43 +0200

    removed the __ in the constructor

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     '''Class rectangle, empty for now'''
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
