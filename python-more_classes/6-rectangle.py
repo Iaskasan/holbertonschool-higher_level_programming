93e5ec8d8522268ab2e2387e347500079a87d580
Iaskasan <nokiesan@yahoo.fr>
2024-05-21 13:34:43 +0200

    missing a blank line(pycodestyle)

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     '''Class rectangle, empty for now'''
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
