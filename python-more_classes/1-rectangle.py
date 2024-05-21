a750890ca7e04c5b3b5ce18e885857642bc19e2b
Iaskasan <nokiesan@yahoo.fr>
2024-05-21 11:17:39 +0200

    added the properties for width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+'''Module of the class rectangle'''
+
+
+class Rectangle:
+    '''Class rectangle, empty for now'''
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
