ae766b7648e2214d7b1eef5c658f52e81ff5da42
Iaskasan <nokiesan@yahoo.fr>
2024-05-21 11:40:23 +0200

    fixed wrong condition for perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -36,6 +36,6 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
-        if self.width or self.height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         return (self.width + self.height) * 2
