166d20b5bce697136edbab21f02c3df2b9e3773c
Iaskasan <nokiesan@yahoo.fr>
2024-05-21 15:17:08 +0200

    added doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+'''
+module for the method lookup that returns a list of
+all attributes and and methods of an object
+'''
+
 
 def lookup(obj):
+    '''returns a list of method and attr of an obj'''
     return dir(obj)
