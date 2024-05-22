7de4abcf735f817ac738001789e9a00edbc02cb2
Iaskasan <nokiesan@yahoo.fr>
2024-05-22 20:44:51 +0200

    added the abstract class module

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''This is a module for the abstract class Animal'''
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    '''This is the parent class Animal'''
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    '''Class Dog that inherits from the class Animal'''
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    '''Class Cat that inherits from the class Animal'''
+    def sound(self):
+        return "Meow"
