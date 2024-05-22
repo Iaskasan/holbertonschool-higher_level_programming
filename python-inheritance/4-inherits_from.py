8de7f50f461114b74ba0aaea22f54a386a81b9ac
Iaskasan <nokiesan@yahoo.fr>
2024-05-22 13:45:31 +0200

    added the inherit method

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''this is the module for the inherits_from method'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    return True if the object
+    is a subclass of the class a_class
+    '''
+    return issubclass(obj, a_class)
