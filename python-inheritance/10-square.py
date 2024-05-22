beffaa5a3a3e2636544d5c5bd7408b94ab6d384b
Iaskasan <nokiesan@yahoo.fr>
2024-05-22 17:16:57 +0200

    added the super() method to get the method from parent classes

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'''Module for the class Square'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class Square that inherits from class Rectangle'''
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
