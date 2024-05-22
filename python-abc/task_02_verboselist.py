76b5d69b2a709deaf218d58a977c40e75ff2e2c9
Iaskasan <nokiesan@yahoo.fr>
2024-05-22 22:00:10 +0200

    added the class VerboseList that modifies the behavior of some methods of the built-in list class

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+'''Class VerboseList that will extend/modify the class list'''
+
+
+class VerboseList(list):
+    '''This class rewrite some methods of the in-built class list'''
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, item):
+        super().extend(item)
+        print(f"Extended the list with [{len(item)}] items.")
+
+    def remove(self, item):
+        super().remove(item)
+        print(f"Removed [{item}] from the list.")
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print(f"Popped [{item}] from the list.")
