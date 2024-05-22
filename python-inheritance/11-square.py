66a5e53bd39a5a2a3317d4d70bce69a38703dbb8
Iaskasan <nokiesan@yahoo.fr>
2024-05-22 17:19:05 +0200

    the __str__ method

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''Module for the class Square'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''class Square that inherits from class Rectangle'''
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
