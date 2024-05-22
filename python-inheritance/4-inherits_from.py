e106f6626da54f0ff5b25a73aa6c71ff9f82c0fb
Iaskasan <nokiesan@yahoo.fr>
2024-05-22 13:51:32 +0200

    added condition for the same class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,4 +7,5 @@ def inherits_from(obj, a_class):
     return True if the object
     is a subclass of the class a_class
     '''
-    return issubclass(type(obj), a_class)
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)
