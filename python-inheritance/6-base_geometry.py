c8dade4a58ffa9da04b02b142b7dab53b7b863ae
Iaskasan <nokiesan@yahoo.fr>
2024-05-22 13:56:41 +0200

    added the empty method area

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''Module for the class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''Class BaseGeometry'''
+    def area(self):
+        '''raise an exception for now'''
+        raise Exception("area() is not implemented")
