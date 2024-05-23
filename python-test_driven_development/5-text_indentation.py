95862011091ba373722dbf46004d1d048bd852b6
Iaskasan <nokiesan@yahoo.fr>
2024-05-23 20:37:43 +0200

    fixed all cases for the text indent

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,10 +6,16 @@ def text_indentation(text):
     '''indent the input string by adding new lines and removing spaces'''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in text:
-        if char in [".", "?", ":"]:
-            print("\n")
-        elif char is " ":
+    char = [".", "?", ":"]
+    for i in range(len(text)):
+        if text[i] in char:
+            print(text[i])
+            print()
+        elif text[i] is " " and text[i - 1] is " ":
+            continue
+        elif text[i] is " " and text[i - 1] not in char:
+            print(" ", end="")
+        elif text[i] is " " and text[i - 1] in char:
             continue
         else:
-            print(char, end="")
+            print(text[i], end="")
