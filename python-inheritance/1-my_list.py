564f81832f360ccdc7b93374bdf0c139e0bbe4a9
Iaskasan <nokiesan@yahoo.fr>
2024-05-23 19:04:26 +0200

    updated txt file and method to return a new list correctly

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,10 @@ class MyList(list):
     '''class MyList'''
 
     def print_sorted(self):
-        new_list = print(sorted(self))
+        new_list = sorted(self)
+        print(new_list)
         return new_list
+
+new = MyList.print_sorted([1, 2, 4, 6])
+# [1, 2, 4, 6]
+print(isinstance(new, list))
