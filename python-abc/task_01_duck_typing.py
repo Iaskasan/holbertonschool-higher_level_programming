fd0078b8cc36ea045ffc8253ebc65b992cbf9dcb
Iaskasan <nokiesan@yahoo.fr>
2024-05-23 15:15:16 +0200

    Handled the negative radius by converting it to an absolute number

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -18,17 +18,7 @@ class Shape(ABC):
 class Circle(Shape):
     '''Class Circle that inherits from the abstract class Shape'''
     def __init__(self, radius):
-        self.radius = radius
-
-    @property
-    def radius(self):
-        return self.radius
-
-    @radius.setter
-    def radius(self, value):
-        if value < 0:
-            raise ValueError("width cannot be negative")
-        self.radius = value
+        self.radius = abs(radius)
 
     def area(self):
         return pi * self.radius ** 2
