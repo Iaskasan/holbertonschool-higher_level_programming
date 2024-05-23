377f84b4c1860b15fb974315f36c5731abf83a60
Iaskasan <nokiesan@yahoo.fr>
2024-05-23 19:05:14 +0200

    oops, removed the test code

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,7 +9,3 @@ class MyList(list):
         new_list = sorted(self)
         print(new_list)
         return new_list
-
-new = MyList.print_sorted([1, 2, 4, 6])
-# [1, 2, 4, 6]
-print(isinstance(new, list))
