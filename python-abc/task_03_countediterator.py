a8e961d254c591a4cad731b834c29f83f806d060
Iaskasan <nokiesan@yahoo.fr>
2024-05-24 14:55:34 +0200

    fixed some cases

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -5,22 +5,26 @@
 class CountedIterator:
     def __init__(self, obj):
         self.obj = obj
-        self.iter = iter(obj)
+        self.iter = 0
         self.count = 0
 
     def __next__(self):
-        self.count += 1
-        if self.count > len(self.obj):
+        if self.iter > len(self.obj):
             raise StopIteration
+        self.iter += 1
+        self.count += 1
         return next(self.iter)
 
     def get_count(self):
         return self.count
 
+    def __iter__(self):
+        return self
+
 
 if __name__ == "__main__":
 
-    data = [1, 2, 3, 4]
+    data = None
 counted_iter = CountedIterator(data)
 
 try:
