7c542f6d5ee019a25906fcb5ca817efff9562edf
Iaskasan <nokiesan@yahoo.fr>
2024-05-24 22:08:36 +0200

    added the dragon module

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+
+class SwimMixin:
+    ''' class SwimMixin'''
+    def swim(self):
+        print("The creature swims!")
+
+    def fly(self):
+        print("The creature flies!")
+
+
+class FlyMixin:
+    ''' class FlyMixin'''
+    def swim(self):
+        print("The creature swims!")
+
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(FlyMixin, SwimMixin):
+    '''
+    Sibling class Dragon that inehrits
+    both from FlyMixin and SwimMiwin
+    '''
+    def roar(self):
+        print("Roooooaaarrr!")
