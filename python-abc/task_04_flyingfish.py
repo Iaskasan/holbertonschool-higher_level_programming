07b42d5695f4aa1cb54c320231124685258f186b
Iaskasan <nokiesan@yahoo.fr>
2024-05-24 15:33:05 +0200

    forgot exclamation point for 2 methods

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -24,7 +24,7 @@ class FlyingFish(Fish, Bird):
         print("The flying fish is soaring!")
 
     def swim(self):
-        print("The flying fish is swimming")
+        print("The flying fish is swimming!")
 
     def habitat(self):
-        print("The flying fish lives both in water and the sky")
+        print("The flying fish lives both in water and the sky!")
