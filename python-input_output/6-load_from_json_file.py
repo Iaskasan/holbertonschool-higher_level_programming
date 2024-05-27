105392f57a1672edbed68fb04c5d118dcc57ee8f
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 11:05:13 +0200

    reverted changes

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,5 +7,4 @@ def load_from_json_file(filename):
     '''function that loads a json file and returns an object of it'''
     with open(filename, "r") as f:
         obj = json.load(f)
-        f.close()
         return obj
