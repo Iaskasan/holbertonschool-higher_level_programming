42623f2f86607d62974baf6375a963dad7c510a9
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 13:52:39 +0200

    added the class student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''Module for the class Student'''
+
+
+class Student:
+    '''class Students'''
+    def __init__(self, first_name, last_name, age):
+        '''the constructor'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''jason et la toison d'or'''
+        return vars(self)
