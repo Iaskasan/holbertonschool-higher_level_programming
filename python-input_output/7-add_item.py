8fd62eef0c22f6f13e6b276c74ce413bb11dcc51
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 11:59:56 +0200

    added the load add save script

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 '''Adonis le plus beau'''
+from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-import json
 
 
-
+python_list = []
+try:
+    python_list = load_from_json_file("add_item.json")
+finally:
+    for args in argv[1:]:
+        python_list.append(args)
+save_to_json_file(python_list, "add_item.json")
