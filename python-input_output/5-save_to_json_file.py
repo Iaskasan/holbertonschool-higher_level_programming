71a062c9ae6b6a2dc61520a345c2779f8d5d6d7b
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 10:22:55 +0200

    added the save to json function

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''Bonjour à toutes et à tous'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''function that saves a file to a json object'''
+    with open(filename, "w") as f:
+        my_obj = json.dumps(my_obj)
+        return f.write(my_obj)
