6cf66bc50a9beb8156ca69b22c015369215087b5
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 09:57:54 +0200

    added first task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+'''Lucas m'a dit de mettre un commentaire'''
+
+
+def read_file(filename=""):
+    '''et ici aussi'''
+    with open(filename, "r") as f:
+        print(f.read(), end="")
