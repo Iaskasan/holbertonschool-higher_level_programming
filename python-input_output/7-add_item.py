11b673cf12eca0dad082cd595d0d8f5cfadd60d4
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 12:03:20 +0200

    added the error handling for the first use of the script

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,11 +4,12 @@ from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-
 python_list = []
 try:
     python_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    pass
 finally:
     for args in argv[1:]:
         python_list.append(args)
-save_to_json_file(python_list, "add_item.json")
+    save_to_json_file(python_list, "add_item.json")
