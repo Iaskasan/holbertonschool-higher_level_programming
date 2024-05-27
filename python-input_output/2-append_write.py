e7931114b827ee52481c222ba35da8a53ad9d77f
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 10:05:43 +0200

    added the append function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''module for the function append on a file'''
+
+
+def append_write(filename="", text=""):
+    '''function that appends text in a file'''
+    with open(filename, "a") as f:
+        f.write(text)
+        return len(text)
