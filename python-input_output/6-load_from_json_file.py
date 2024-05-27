03bd5a5e26d13f153012c40fd31a25d59d6f8271
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 11:20:05 +0200

    added the encoding

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     '''function that loads a json file and returns an object of it'''
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         obj = json.load(f)
         return obj
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+'''Adonis le plus beau'''
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import json
+
+
+
