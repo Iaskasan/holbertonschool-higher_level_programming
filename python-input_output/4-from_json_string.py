2f924d2983750667d76b47c6a910cb8bc68d06ed
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 10:17:30 +0200

    added the json string to python dict function

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+'''json json jason'''
+import json
+
+
+def from_json_string(my_str):
+    '''function that retrieves a json string to python format'''
+    return json.loads(my_str)
