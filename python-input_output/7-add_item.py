5a2b7a56131868bf1ddc860b00670d3e8dd507a3
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 13:45:14 +0200

    added vars method

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+
 python_list = []
 try:
     python_list = load_from_json_file("add_item.json")
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+'''Class to JSON module'''
+
+
+def class_to_json(obj):
+    '''Lucas veut toujours aram'''
+    return vars(obj)
