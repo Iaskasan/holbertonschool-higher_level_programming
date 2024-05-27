f744a89e1fb7861c6629c89dd36be086c2120343
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 10:59:08 +0200

    added load from json to obj function

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''comments comments comments'''
+import json
+
+
+def load_from_json_file(filename):
+    '''function that loads a json file and returns an object of it'''
+    with open(filename, "r") as f:
+        obj = f.read()
+        return json.loads(obj)
