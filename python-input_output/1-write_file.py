b1e02970c378ae9ca1e561755974f783c913adb1
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 10:03:25 +0200

    added the write file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+'''Module for the function that write in a file'''
+
+
+def write_file(filename="", text=""):
+    '''writes in a file'''
+    with open(filename, "w") as f:
+        return f.write(text)
