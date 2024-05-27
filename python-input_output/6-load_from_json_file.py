e957e6069741485f0a7384ae94ad1bd9339cdaf2
Iaskasan <nokiesan@yahoo.fr>
2024-05-27 11:03:46 +0200

    reworked the code

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     '''function that loads a json file and returns an object of it'''
     with open(filename, "r") as f:
-        obj = f.read()
-        return json.loads(obj)
+        obj = json.load(f)
+        return obj
