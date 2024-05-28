20bb1e778f2811d72ecf85fe784dd0bc38153176
Iaskasan <nokiesan@yahoo.fr>
2024-05-28 14:49:03 +0200

    added docstring for methods

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,16 +6,19 @@ class CustomObject:
     '''class customobject using pickle module'''
 
     def __init__(self, name: str, age: int, is_student: bool):
+        '''constructor for the class CustomObject'''
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
+        '''method display, prints the instances of the class'''
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
+        '''method that serializes the object into a file'''
         try:
             with open(filename, "wb") as f:
                 pickle.dump(self, f)
@@ -24,6 +27,7 @@ class CustomObject:
 
     @classmethod
     def deserialize(cls, filename):
+        '''method that deserializes an object from a file'''
         try:
             with open(filename, "rb") as f:
                 obj = pickle.load(f)
