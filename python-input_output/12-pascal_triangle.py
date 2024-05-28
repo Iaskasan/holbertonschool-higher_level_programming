4246402c06277fd180113fbcf4e6c25a246ecc84
Iaskasan <nokiesan@yahoo.fr>
2024-05-28 11:43:13 +0200

    added the pascal triangle function

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''function that creates a pascal triangle'''
+
+
+def pascal_triangle(n):
+    '''n is the number of row of the pascal triangle'''
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
