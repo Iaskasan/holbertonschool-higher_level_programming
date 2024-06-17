e19ae600e12f3300182c2dc4378fa5a2b6f00496
Iaskasan <nokiesan@yahoo.fr>
2024-06-17 15:03:59 +0200

    added all files

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- create a table called first_table in the current database
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,1 @@
+-- print the following description of the table first_table from the database hbtn_0c_0
