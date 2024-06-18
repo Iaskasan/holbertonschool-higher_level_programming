111e5ba2bd14a98a8f4f6b5793afb3247d1e6aa6
Iaskasan <nokiesan@yahoo.fr>
2024-06-18 15:37:10 +0200

    added 9th task

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+--  list all the cities of California that can be found in the database hbtn_0d_usa
+SELECT name
+FROM hbtn_0d_usa.cities
+WHERE state_id = (
+    SELECT id
+    FROM hbtn_0d_usa.states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
