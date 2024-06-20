dba3afe63319615ec30628f395b83fea7e878002
Iaskasan <nokiesan@yahoo.fr>
2024-06-20 11:09:12 +0200

    added README.md and first task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+"""Module that prints all states by ASCENDING order"""
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=mysql_username,
+                        password=mysql_password,
+                        database=database_name
+                        )
+
+    c = db.cursor()
+    c.execute("""SELECT * from states ORDER BY name ASC""")
+
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
