6cf6add40cfd6d310838453aad142bc5e0aa5fcf
Iaskasan <nokiesan@yahoo.fr>
2024-06-20 14:25:34 +0200

    added names startijg by N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Module that prints all states by ASCENDING order"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Get MySQL username, password, and database
+    name from command line arguments
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=mysql_username,
+                        password=mysql_password,
+                        database=database_name
+                        )
+
+    c = db.cursor()
+    c.execute("""SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC""")
+
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
