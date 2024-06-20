8e7d26901e3c9a188964c2b1af587f224a9a5b41
Iaskasan <nokiesan@yahoo.fr>
2024-06-20 11:12:27 +0200

    added more doc

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,12 @@ import sys
 """Module that prints all states by ASCENDING order"""
 
 if __name__ == "__main__":
+    # Get MySQL username, password, and database name from command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
+    # Connect to the MySQL database
     db = MySQLdb.connect(
                         host="localhost",
                         port=3306,
@@ -16,13 +18,18 @@ if __name__ == "__main__":
                         database=database_name
                         )
 
+    # Create a cursor object to interact with the database
     c = db.cursor()
+    # Execute the SQL query to select all states and order them by name in ascending order
     c.execute("""SELECT * from states ORDER BY name ASC""")
 
+    # Fetch all the results of the query
     rows = c.fetchall()
 
+    # Print each row in the result set
     for row in rows:
         print(row)
 
+    # Close the cursor and the database connection
     c.close()
     db.close()
