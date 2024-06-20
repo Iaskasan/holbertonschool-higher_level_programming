d6730c3be180e4fc9bd7f8715bcb857be996ab2e
Iaskasan <nokiesan@yahoo.fr>
2024-06-20 15:02:32 +0200

    added the filter with argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""Module that prints all states by ASCENDING order"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Get MySQL username, password, and database
+    name from command line arguments
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=mysql_username,
+                        password=mysql_password,
+                        database=database_name
+                        )
+
+    c = db.cursor()
+    c.execute(f"""SELECT * FROM states WHERE
+              name LIKE '{state_name_searched}' ORDER BY id ASC""")
+
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
