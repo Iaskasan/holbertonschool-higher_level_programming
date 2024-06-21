8fedd03bf43af49faa8afcfc266d4a920ab9bc33
Iaskasan <nokiesan@yahoo.fr>
2024-06-21 11:29:31 +0200

    added the first model state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Module that defines a class State"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+PORT = 3306
+HOST = 'localhost'
+
+
+class State(Base):
+    """Class State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
