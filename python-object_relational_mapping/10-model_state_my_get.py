2c5599704605f79b487b7ab17b322c06834fc08b
Iaskasan <nokiesan@yahoo.fr>
2024-06-21 12:45:57 +0200

    added the state as argument

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""Module that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """
+    Fetch all states from the database hbtn_0e_6_usa
+    """
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(mysql_username,
+                                   mysql_password,
+                                   database_name),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = sessionmaker()(bind=engine)
+
+    state = session.query(State).order_by(State.id).all()
+
+    for state in state:
+        if sys.argv[4] in state.name:
+            print(state.id)
+            break
+    else:
+        print("Not found")
+    # for state in states:
+    #     if "a" in state.name:
+    #         print("{}: {}".format(state.id, state.name))
+
+    session.close()
