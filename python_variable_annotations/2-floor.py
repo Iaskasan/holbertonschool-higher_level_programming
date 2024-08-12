2785287a9d3d90740d9fe4ab871421fa43c614b3
Iaskasan <nokiesan@yahoo.fr>
2024-08-12 12:44:27 +0200

    added first 8 tasks

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,3 @@
 #!/usr/bin/env python3
+def floor(n: float) -> int:
+    return int(n)
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+    return multiplier_function
+
+print(make_multiplier.__annotations__)
+fun = make_multiplier(2.22)
+print("{}".format(fun(2.22)))
